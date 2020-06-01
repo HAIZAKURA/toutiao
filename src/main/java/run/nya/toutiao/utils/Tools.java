a05262806f592ae2ba99538a3226601fffdd37d3
HAIZAKURA <hhbilly99@gmail.com>
2020-06-01 22:56:57 +0800

    Create Tools

---
+++ src/main/java/run/nya/toutiao/utils/Tools.java
@@ -0,0 +1,49 @@
+package run.nya.toutiao.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Tools {
+
+    /**
+     * @method getMD5
+     * @remark 计算字符串MD5值
+     * @param  plainText String
+     * @return res
+     */
+    public static String getMD5(String plainText) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("md5");
+            byte[] procBytes = plainText.getBytes();
+            byte[] digest = md5.digest(procBytes);
+            char[] chars = new char[] {'0', '1', '2', '3', '4', '5', '6',
+                    '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+            StringBuilder procSB = new StringBuilder();
+            for (byte eachByte : digest) {
+                procSB.append(chars[(eachByte >> 4) & 15]);
+                procSB.append(chars[eachByte & 15]);
+            }
+            return procSB.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * @method getNowTime
+     * @remark 获取当前时间 (yyyy-MM-dd HH:mm:ss)
+     * @return res
+     */
+    public static String getNowTime() {
+        String res;
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        res = sdf.format(date);
+        return res;
+    }
+
+}
