03ff556ace6f496c6b83a0c4ffe8dcc9d0446df3
HAIZAKURA <hhbilly99@gmail.com>
2020-06-01 17:04:47 +0800

    Default Changelist

---
+++ src/main/java/run/nya/toutiao/ToutiaoApplication.java
@@ -0,0 +1,13 @@
+package run.nya.toutiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToutiaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToutiaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/run/nya/toutiao/ToutiaoApplicationTests.java
@@ -0,0 +1,13 @@
+package run.nya.toutiao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToutiaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
