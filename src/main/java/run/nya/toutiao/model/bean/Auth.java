90571bcb7b7c1976016da0ec89a472be38064880
HAIZAKURA <hhbilly99@gmail.com>
2020-06-09 21:48:01 +0800

    create Auth.java

---
+++ src/main/java/run/nya/toutiao/model/bean/Auth.java
@@ -0,0 +1,37 @@
+package run.nya.toutiao.model.bean;
+
+public class Auth {
+
+    // 权限ID
+    private Integer aid;
+
+    // 权限名
+    private String aname;
+
+    // 权限描述
+    private String adesc;
+
+    public Integer getAid() {
+        return aid;
+    }
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getAdesc() {
+        return adesc;
+    }
+
+    public void setAdesc(String adesc) {
+        this.adesc = adesc;
+    }
+}
