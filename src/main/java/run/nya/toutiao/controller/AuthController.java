24cd2c3969a3db3b4f91006e27724f7543b1fd3f
HAIZAKURA <hhbilly99@gmail.com>
2020-06-12 11:50:13 +0800

    add modAuth & use fastjson

--- src/main/java/run/nya/toutiao/controller/AuthController.java
+++ src/main/java/run/nya/toutiao/controller/AuthController.java
@@ -1,21 +1,23 @@
 package run.nya.toutiao.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import run.nya.toutiao.model.bean.Auth;
 import run.nya.toutiao.model.dao.AuthDao;
+import run.nya.toutiao.utils.Checker;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@Api(description = "Auth Operation Interface")
+@Api(tags = {"Auth"})
 public class AuthController {
 
     /**
@@ -33,20 +35,21 @@ public class AuthController {
      * @access ALL
      * @method GET
      * @route  /api/auth
-     * @return res
+     * @return res JSONString
      */
-    @RequestMapping(value = "/api/auth", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/auth", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     @ApiOperation(value = "Get All Auth Information", httpMethod = "GET", notes = "ALL")
     public Object getAllAuth() {
-        Map<String, Object> res = new HashMap<>();
+        JSONObject res = new JSONObject();
         try {
             List<Auth> authList = authDao.getAllAuth();
             res.put("code", 1);
             res.put("data", authList);
         } catch (Exception e) {
+            e.printStackTrace();
             res.put("code", 0);
         }
-        return res;
+        return res.toJSONString();
     }
 
     /**
@@ -56,20 +59,21 @@ public class AuthController {
      * @method GET
      * @route  /api/auth/{aid}
      * @param  aid Integer
-     * @return res
+     * @return res JSONString
      */
-    @RequestMapping(value = "/api/auth/{aid}", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/auth/{aid}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     @ApiOperation(value = "Get an Auth Information By ID", httpMethod = "GET", notes = "ALL")
-    public Object getAuthById(@PathVariable(value = "aid") Integer aid) {
-        Map<String, Object> res = new HashMap<>();
+    public String getAuthById(@PathVariable(value = "aid") Integer aid) {
+        JSONObject res = new JSONObject();
         try {
             Auth auth = authDao.getAuthById(aid);
             res.put("code", 1);
             res.put("data", auth);
         } catch (Exception e) {
+            e.printStackTrace();
             res.put("code", 0);
         }
-        return res;
+        return res.toJSONString();
     }
 
     /**
@@ -78,16 +82,38 @@ public class AuthController {
      * @access Admin
      * @method PUT
      * @route  /api/auth/{aid}
-     * @param  aid   Integer
-     * @param  aname String
-     * @param  adesc String
-     * @return res
+     * @param  aid     Integer
+     * @param  body    JSONObject
+     * @param  session HttpSession
+     * @return res     JSONString
      */
-    @RequestMapping(value = "/api/auth/{aid}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/api/auth/{aid}", method = RequestMethod.PUT, produces = "application/json;charset=UTF-8")
     @ApiOperation(value = "Modify an Auth Information By ID", httpMethod = "PUT", notes = "Admin")
-    public Object modAuthById(@PathVariable(value = "aid") Integer aid, String aname, String adesc) {
-        Map<String, Object> res = new HashMap<>();
-        return res;
+    public String modAuthById(@PathVariable(value = "aid") Integer aid, @RequestBody JSONObject body,
+                              HttpSession session) {
+        JSONObject res = new JSONObject();
+        Auth reqAuth = JSON.parseObject(body.toJSONString(), Auth.class);
+        if (Checker.isAdmin(session)) {
+            try {
+                Integer back = authDao.modAuth(aid, reqAuth.getAname(), reqAuth.getAdesc());
+                if (back > 0) {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("aid", aid);
+                    data.put("aname", reqAuth.getAname());
+                    data.put("adesc", reqAuth.getAdesc());
+                    res.put("code", 1);
+                    res.put("data", data);
+                } else {
+                    res.put("code", 0);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                res.put("code", 0);
+            }
+        } else {
+            res.put("code", -1);
+        }
+        return res.toJSONString();
     }
 
 }
