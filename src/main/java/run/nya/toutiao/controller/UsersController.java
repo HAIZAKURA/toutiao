a90e6485a642417d36c50a9cb6a51d628a4d51ba
HAIZAKURA <hhbilly99@gmail.com>
2020-06-14 12:16:20 +0800

    update UsersController.java

--- src/main/java/run/nya/toutiao/controller/UsersController.java
+++ src/main/java/run/nya/toutiao/controller/UsersController.java
@@ -4,11 +4,12 @@ import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import run.nya.toutiao.model.bean.Users;
 import run.nya.toutiao.model.dao.UsersDao;
+import run.nya.toutiao.utils.Checker;
+
+import java.util.List;
 
 @RestController
 @Api(tags = {"Users"})
@@ -37,6 +38,80 @@ public class UsersController {
     @ApiOperation(value = "User Login", httpMethod = "POST", notes = "ALL")
     public String userLogin(@RequestBody JSONObject body) {
         JSONObject res = new JSONObject();
+        return res.toJSONString();
+    }
+
+    /**
+     * @api    userRegister
+     * @remark 用户注册
+     * @access ALL
+     * @method POST
+     * @route  /api/register
+     * @param  body JSONObject
+     * @return res  JSONString
+     */
+    @RequestMapping(value = "/api/register", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    @ApiOperation(value = "User Register", httpMethod = "POST", notes = "ALL")
+    public String userRegister(@RequestBody JSONObject body) {
+        JSONObject res = new JSONObject();
+        return res.toJSONString();
+    }
+
+    /**
+     * @api    getAllUsers
+     * @remark 获取所有用户信息
+     * @access ALL
+     * @method GET
+     * @route  /api/users
+     * @return res JSONString
+     */
+    @RequestMapping(value = "/api/users", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ApiOperation(value = "Get All Users Information", httpMethod = "GET", notes = "ALL")
+    public String getAllUsers() {
+        JSONObject res = new JSONObject();
+        try {
+            List<Users> usersList = usersDao.getAllUsers();
+            res.put("code", 1);
+            res.put("data", usersList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.put("code", 0);
+        }
+        return res.toJSONString();
+    }
+
+    /**
+     * @api    getUserBy
+     * @remark 获取用户信息 通过uid/uname
+     * @access ALL
+     * @method GET
+     * @route  /api/users/{value}
+     * @param  value String
+     * @return res   JSONString
+     */
+    @RequestMapping(value = "/api/users/{value}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ApiOperation(value = "Get User Information By ID or Name", httpMethod = "GET", notes = "ALL")
+    public String getUserBy(@PathVariable("value") String value) {
+        JSONObject res = new JSONObject();
+        if (Checker.isStartNum(value)) {
+            try {
+                Users users = usersDao.getUserById(Integer.valueOf(value));
+                res.put("code", 1);
+                res.put("data", users);
+            } catch (Exception e) {
+                e.printStackTrace();
+                res.put("code", 0);
+            }
+        } else {
+            try {
+                Users users = usersDao.getUserByName(value);
+                res.put("code", 1);
+                res.put("data", users);
+            } catch (Exception e) {
+                e.printStackTrace();
+                res.put("code", 0);
+            }
+        }
         return res.toJSONString();
     }
 
