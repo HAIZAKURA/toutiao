776362081f6bcd154a8a2d7729423f95feb5d229
HAIZAKURA <hhbilly99@gmail.com>
2020-06-16 22:09:45 +0800

    add new func

--- src/main/java/run/nya/toutiao/model/dao/RateDao.java
+++ src/main/java/run/nya/toutiao/model/dao/RateDao.java
@@ -10,4 +10,10 @@ public interface RateDao {
     // 获取评分
     Rate getRate(@Param("tid") Integer tid);
 
+    // 点赞
+    Integer addGood(@Param("tid") Integer tid);
+
+    // 点踩
+    Integer addBad(@Param("tid") Integer tid);
+
 }
