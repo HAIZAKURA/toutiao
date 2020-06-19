c41e71dcc85ea3a825b62119c4c104f1ea5e19be
HAIZAKURA <hhbilly99@gmail.com>
2020-06-19 21:43:37 +0800

    create NoticeDao.java

--- src/main/java/run/nya/toutiao/model/dao/NoticeDao.java
+++ src/main/java/run/nya/toutiao/model/dao/NoticeDao.java
@@ -1,4 +1,30 @@
 package run.nya.toutiao.model.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import run.nya.toutiao.model.bean.Notice;
+
+import java.util.List;
+
+@Mapper
 public interface NoticeDao {
+
+    // 获取所有通知
+    List<Notice> getAllNotice();
+
+    // 获取用户所有通知
+    List<Notice> getUserNotice(@Param("uid") Integer uid);
+
+    // 获取通知
+    Notice getNotice(@Param("nid") Integer nid);
+
+    // 添加通知
+    Integer addNotice(@Param("ncont") String ncont, @Param("uid") Integer uid);
+
+    // 修改通知
+    // Integer modNotice(@Param("nid") Integer nid, @Param("ncont") String ncont);
+
+    // 删除通知
+    Integer delNotice(@Param("nid") Integer nid);
+
 }
